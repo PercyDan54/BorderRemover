23f07633b329e21f1c2d07dae9f33aa86f06fbac
PercyDan <50285552+PercyDan54@users.noreply.github.com>
2021-07-24 11:56:54 +0800

    How the fuck did this work

--- src/main/java/me/percydan/borderremover/mixins/MixinWorld.java
+++ src/main/java/me/percydan/borderremover/mixins/MixinWorld.java
@@ -1,5 +1,6 @@
 package me.percydan.borderremover.mixins;
 
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.World;
@@ -13,12 +14,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(World.class)
 public abstract class MixinWorld implements WorldAccess {
     @Inject(method = "isValidHorizontally", at = @At("RETURN"), cancellable = true)
-    private static void isValidHorizontally(CallbackInfoReturnable<Boolean> ci) {
+    private static void isValidHorizontally(BlockPos pos, CallbackInfoReturnable<Boolean> ci) {
         ci.setReturnValue(true);
     }
 
     @Inject(method = "isInvalidVertically", at = @At("RETURN"), cancellable = true)
-    private static void isInvalidVertically(CallbackInfoReturnable<Boolean> ci) {
+    private static void isInvalidVertically(int y, CallbackInfoReturnable<Boolean> ci) {
         ci.setReturnValue(false);
     }
 
